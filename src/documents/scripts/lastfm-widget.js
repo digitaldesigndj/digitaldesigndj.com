9edd429fae11551002307eba5b5b7fb848810dfb
Taylor Young <taylor@x-team.com>
2013-05-09 16:56:21 -0500

    salted lastfm feed url, stops google feed from caching

--- src/documents/scripts/lastfm-widget.js
+++ src/documents/scripts/lastfm-widget.js
@@ -1,17 +1,19 @@
 $(function(){
 	if( $('.lastfm').length !== 0 ) {
 		var template = Handlebars.templates['lastfm-widget-template'];
+		var salt = new Date().getTime();
 		// http://stackoverflow.com/questions/226663/parse-rss-with-jquery
 		function parseRSS(url, callback) {
 			$.ajax({
-				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-				dataType: 'json',
-				success: function(data) {
+				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url)
+				, dataType: 'json'
+				, cache: false
+				, success: function(data) {
 					callback(data.responseData.feed);
 				}
 			});
 		}
-		parseRSS('http://ws.audioscrobbler.com/2.0/user/DigitalDesignDj/recenttracks.rss', function(data){
+		parseRSS('http://ws.audioscrobbler.com/2.0/user/DigitalDesignDj/recenttracks.rss?nocache='+salt, function(data){
 			// console.log( data );
 			$('.lastfm').append( template(data) );
 		});
