3b5569b105abcedfcada3a107c4f7153c07f151d
Taylor Young <taylor@x-team.com>
2013-05-24 21:50:48 -0500

    local to live connection

---
+++ src/documents/js/lastfm-widget.js
@@ -0,0 +1,24 @@
+define(['jquery', 'socket-io', 'hbs!hb/lastfm-recent', 'hbs!hb/lastfm-current'], function($, io, tmplRecent, tmplCurrent) {
+	var widgets, socket, lastfm;
+	widgets        = $( '.lastfm' )
+	socket         = io.connect( 'http://192.168.0.7:8880', { resource: 'api/socket.io' });
+	// socket         = io.connect( 'http://digitaldesigndj.com', { resource: 'api/socket.io' });
+	lastfm         = function() {
+		$.each(widgets, function( i, v ) {
+			var template, widget;
+			widget = $( v );
+			if (widget.length) {
+				if (widget.hasClass( 'current' ) ) {
+					template = tmplCurrent;
+				} else {
+					template = tmplRecent;
+				}
+				socket.of('/api').on( 'update', function( data ) {
+					console.log(data);
+					widget.html( template( data ) );
+				});
+			}
+		});
+	};
+	lastfm();
+});
