1ea2312572860e37ceb9dafec897bb85a792f8fc
Taylor Young <taylor@x-team.com>
2013-05-26 08:30:16 -0500

    page transitions

--- src/documents/js/main.js
+++ src/documents/js/main.js
@@ -10,4 +10,15 @@ define([ 'socket-io', 'lastfm-widget' ], function( io, lastfmWidget ) {
 	socket.of('/api').on( 'broadcast', function( data ) {
 		$('#broadcast').text( data );
 	});
+	$('.outer').animate({'opacity':'1'}, 250);
+	// Animate Out
+	$("a").on("click", function(){
+		var href = $(this).attr("href");
+		var animDuration = 250;
+		$('.outer').animate({'opacity':'0'}, animDuration);
+		setTimeout(function () {
+			window.location = href;
+		}, animDuration);
+		return false;
+	});
 });
