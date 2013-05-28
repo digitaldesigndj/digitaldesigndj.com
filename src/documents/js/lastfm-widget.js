7c1fb34ef20b12be4908e981f088e674d9015327
Taylor Young <taylor@x-team.com>
2013-05-27 21:51:50 -0500

    whitespace

--- src/documents/js/lastfm-widget.js
+++ src/documents/js/lastfm-widget.js
@@ -1,10 +1,10 @@
 define([ 'jquery', 'hbs!hb/lastfm-recent', 'hbs!hb/lastfm-current' ]
-	, function($, tmplRecent, tmplCurrent) {
+	, function( $, tmplRecent, tmplCurrent ) {
 		return function widget( data ) {
 			$.each( $( '.lastfm' ) , function( i, v ) {
 				var widget = $( v );
 				if ( widget.length ) {
-					if( widget.hasClass( 'current' ) ){
+					if( widget.hasClass( 'current' ) ) {
 						if( data.recenttracks.track.length === 11 ) {
 							widget
 								.html( tmplCurrent( data ) )
