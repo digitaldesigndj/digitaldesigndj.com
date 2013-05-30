6bde7d004f78353dde84c01997586f76b39c3ec8
Taylor Young <taylor@x-team.com>
2013-05-30 16:59:18 -0500

    amd style google analytics

---
+++ src/documents/js/analytics.js
@@ -0,0 +1,4 @@
+define([ 'http://www.google-analytics.com/ga.js' ], function ( ga ) {
+	ga = { q: [['create', 'UA-18710277-1', 'jspm.io'], ['send', 'pageview']], l: Date.now() };
+	console.log( ga );
+});
