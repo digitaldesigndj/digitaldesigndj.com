7f6087374356f69e0ce1a4b5df72698b8f50258b
Taylor Young <taylor@x-team.com>
2013-05-29 21:47:01 -0500

    dev require issue looks resolved

--- src/documents/js/app.js
+++ src/documents/js/app.js
@@ -9,24 +9,10 @@ require.config({
 		, 'propertyParser'        : 'plugin/propertyParser'
 		, 'font'                  : 'plugin/font'
 		, 'hbs'                   : 'plugin/hbs'
-		// , 'main'                  : 'main'
-		// , 'scroll-page-offscreen' : 'scroll-page-offscreen'
-		// , 'github-widgets'        : 'github-widgets'
-		// , 'live-reload'           : 'live-reload'
-		// , 'json-widget'           : 'json-widget'
-		// , 'socket-io'             : 'socket-io'
-		// , 'lastfm-widget'         : 'lastfm-widget'
-		// , 'hbs!hb/github-com'     : 'hbs!hb/github-com'
-		// , 'hbs!hb/github-api'     : 'hbs!hb/github-api'
-		// , 'hbs!hb/lastfm-recent'  : 'hbs!hb/lastfm-recent'
-		// , 'hbs!hb/lastfm-current' : 'hbs!hb/lastfm-current'
 	}
 	, shim : {
 		'font' : {
 			deps: [ 'propertyParser' ]
-		}
-		, 'hbs' : {
-			deps : [ 'Handlebars', 'hbs/underscore', 'hbs/i18nprecompile', 'hbs/json2' ]
 		}
 		, 'jquery': {
 			'exports' : '$'
@@ -37,21 +23,6 @@ require.config({
 		, 'socket-io': {
 			'exports' : 'io'
 		}
-		// , 'main' : {
-		// 	deps: [ 'socket-io', 'lastfm-widget' ]
-		// }
-		, 'scroll-page-offscreen' : {
-			deps: [ 'jquery' ]
-		}
-		// , 'github-widget' : {
-		// 	deps: [ 'hbs', 'json-widget', 'hbs!hb/github-com', 'hbs!hb/github-api' ]
-		// }
-		// , 'json-widget' : {
-		// 	deps: [ 'underscore', 'jquery' ]
-		// }
-		// , 'lastfm-widget' : {
-		// 	deps: [ 'hbs', 'jquery', 'hbs!hb/lastfm-recent', 'hbs!hb/lastfm-current' ]
-		// }
 	}
 	, hbs: {
 		'disableI18n'      : true
