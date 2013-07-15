764888964bf2d3d57b155a32d240552490ce7a8e
Taylor Young <taylor@x-team.com>
2013-07-15 13:31:46 -0500

    grunt csslint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,34 @@ module.exports = function(grunt) {
 					{expand: true, cwd: 'out/', src: ['**'], dest: '../digitaldesigndj.com.out'},
 				]
 			}
+		},
+		csslint: {
+			options: {
+			// 	csslintrc: '.csslintrc'
+				formatters: [
+					{id: 'text', dest: 'report/csslint.txt'},
+					{id: 'csslint-xml', dest: 'report/csslint.xml'}
+				]
+			},
+			strict: {
+				options: {
+					import: false
+				},
+				src: ['out/css/*.css']
+			},
+			// lax: {
+			// 	options: {
+			// 		import: false
+			// 	},
+			// 	src: ['out/css/*.css']
+			// }
 		}
 	});
 
 	// Load the plugin that provides the task.
 	grunt.loadNpmTasks('grunt-bower-requirejs');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 
 	// Default task(s).
 	grunt.registerTask('default', ['bower']);
